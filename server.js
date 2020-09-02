b3fc69940d06e58cbc7c548897585e6bbe8fb22d
Andrew Pomaz <pomaz.andrew@gmail.com>
2020-09-02 14:31:56 +0300

    1.0.0

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./src/schema.js');
+const cors = require('cors')
+
+
+let port = 3000;
+const app = express();
+app.use(cors()); 
+
+app.use('/', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+app.listen(port);
+console.log('GraphQL API server running at localhost:'+ port);
